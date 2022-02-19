cd17081751bec31934e9a09efc38f4792c1b268f
Travis Roy <github@travisroy.ca>
2022-02-18 22:22:04 -0500

    feat: add getProfileFromAccountId() function
    Updating comments on function

--- src/user/getProfileFromAccountId.ts
+++ src/user/getProfileFromAccountId.ts
@@ -10,14 +10,9 @@ import { USER_BASE_URL } from "./USER_BASE_URL";
 type GetProfileFromAccountIdOptions = Pick<AllCallOptions, "headerOverrides">;
 
 /**
- * A call to this function will retrieve the profile of the username being requested.
- * If the user cannot be found (either due to non-existence or privacy settings),
+ * A call to this function will retrieve the profile of the accountId being requested.
+ * If the account profile cannot be found (either due to non-existence or privacy settings),
  * an error will be thrown.
- *
- * This is a legacy API endpoint function. If you are just trying to get a user's
- * account ID, [`makeUniversalSearch()`](https://psn-api.achievements.app/api-docs/universal-search#makeuniversalsearch)
- * is recommended instead. This endpoint is here because it can return interesting
- * presence information when the user is playing on a legacy console such as a PS3.
  *
  * @param authorization An object containing your access token, typically retrieved with `exchangeCodeForAccessToken()`.
  * @param accountId The accountId for the user you wish to retrieve a profile for.
